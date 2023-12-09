494fbc571fe34917c740e1c5dae4f54b5ed09118
Meldrick Carabeo <meldrick.carabeo@gmail.com>
2023-12-09 18:31:20 +0800

    Create BankAccountDecoratorInterface.java

---
+++ BankAccountDecoratorInterface.java
@@ -0,0 +1,8 @@
+interface BankAccountDecorator {
+    String showAccountType();
+    double getInterestRate();
+    double getBalance();
+    String showBenefits();
+    double computeBalanceWithInterest();
+    String showInfo();
+}
